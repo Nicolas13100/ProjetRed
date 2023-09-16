f16624222dc8bbdeec63a027f164c71f8070acec
Nicolas <nicolas.moyon@ynov.com>
2023-09-17 00:34:09 +0200

    init of project from tak 1 to 6

--- utility/marchand.go
+++ utility/marchand.go
@@ -17,6 +17,38 @@ func NewMarchand(personnage *game.Personnage) *Marchand {
 	}
 }
 
+func (m *Marchand) Buy(personnage *game.Personnage, item string, price int) {
+	fmt.Println("Les objets disponibles à l'achat sont :")
+	for key, value := range m.inventory {
+		fmt.Println(key, ": ", value)
+	}
+
+	fmt.Println("Que voulez-vous acheter ? (Entrez le nom de l'objet)")
+	var itemToBuy string
+	fmt.Scan(&itemToBuy)
+
+	fmt.Println("User input:", itemToBuy) // Debug statement
+	for key := range m.inventory {
+		fmt.Println("Inventory item:", key) // Debug statement
+	}
+
+	if quantity, ok := m.inventory[itemToBuy]; ok && quantity > 0 {
+		if personnage.Gold >= price {
+			m.inventory[itemToBuy]--
+			personnage.Gold -= price
+			if quantity > 0 {
+				personnage.Inventory[itemToBuy]++
+			} else {
+				personnage.Inventory[itemToBuy] = 1
+			}
+			fmt.Println("Vous avez acheté une", itemToBuy)
+		} else {
+			fmt.Println("Vous n'avez pas assez d'argent pour acheter cet objet.")
+		}
+	} else {
+		fmt.Println("Désolé, je n'ai pas cet objet en stock.")
+	}
+}
 func (m *Marchand) Sell(Personnage *game.Personnage, item string, price int) {
 	fmt.Println("Les objets disponibles à l'achat sont :")
 	for key, value := range m.inventory {
@@ -35,34 +67,6 @@ func (m *Marchand) Sell(Personnage *game.Personnage, item string, price int) {
 			m.inventory[itemToBuy]--
 			Personnage.Gold -= price
 			Personnage.Inventory[itemToBuy]++
-			fmt.Println("Vous avez acheté une", itemToBuy)
-		} else {
-			fmt.Println("Vous n'avez pas assez d'argent pour acheter cet objet.")
-		}
-	} else {
-		fmt.Println("Désolé, je n'ai pas cet objet en stock.")
-	}
-}
-
-func (m *Marchand) Buy(personnage *game.Personnage, item string, price int) {
-	fmt.Println("Les objets disponibles à l'achat sont :")
-	for key, value := range m.inventory {
-		fmt.Println(key, ": ", value)
-	}
-
-	fmt.Println("Que voulez-vous acheter ? (Entrez le nom de l'objet)")
-	var itemToBuy string
-	fmt.Scan(&itemToBuy)
-
-	if quantity, ok := m.inventory[itemToBuy]; ok && quantity > 0 {
-		if personnage.Gold >= 50 { // Assuming a price of 50 gold for a potion
-			m.inventory[itemToBuy]--
-			personnage.Gold -= 50
-			if quantity > 0 {
-				personnage.Inventory[itemToBuy]++
-			} else {
-				personnage.Inventory[itemToBuy] = 1
-			}
 			fmt.Println("Vous avez acheté une", itemToBuy)
 		} else {
 			fmt.Println("Vous n'avez pas assez d'argent pour acheter cet objet.")
