9786b88ca5d6af461e118849d7bf8b7441ac0c48
Alexis <alexis.clerc@ynov.com>
2023-09-22 11:18:08 +0200

    grrrr

--- game/CombatSystem.go
+++ game/CombatSystem.go
@@ -4,76 +4,101 @@ import "fmt"
 
 var Tours int
 
-func Tutorial(P1 *Personnage) {
+func Tutorial(P1 *Personnage, Monstre1 *Monstre) {
 	ClearConsole()
 	fmt.Println("Bienvenue dans le tutoriel")
 	fmt.Println("Nous allons vous apprendre les bases du combat en tour par tour")
 	fmt.Println("Vous allez vous battre contre un goblin d'entrainement")
 	fmt.Println("Bonne chance")
 	Tours = 1
-	goblin := Monstre{
-		Name:       "Goblin d'entrainement",
-		HpMax:      30,
-		Hp:         30,
-		ManaMax:    10,
-		Mana:       10,
-		Atk:        8,
-		Defense:    2,
-		Initiative: 12,
-	}
-	Fight(P1, &goblin)
+	TrainFight(P1, Monstre1)
 }
 
-func Entrainement(P1 *Personnage) {
-	ClearConsole()
-	fmt.Println("Bienvenue dans la zone d'entrainement")
-	fmt.Println("Amusez-vous a tapé le mannequin d'entrainement")
-	Tours = 1
-	mannequin := Monstre{
-		Name:       "Mannequin d'entrainement",
-		Niveau:     1,
-		HpMax:      30,
-		Hp:         30,
-		Defense:    10,
-		Initiative: 0,
-		XpDrop:     20,
-	}
-	Fight(P1, &mannequin)
-
-}
-
-func Fight(P1 *Personnage, Monstre1 *Monstre) {
+func TrainFight(P1 *Personnage, Monstre1 *Monstre) {
 	fmt.Println("Début du combat d'entrainement")
-
-	for P1.Hp > 0 && Monstre1.Hp > 0 {
+	for (P1.Hp > 0 && Monstre1.Hp > 0 && Tours == 1 && P1.Initiative >= Monstre1.Initiative) || (Tours > 1 && P1.Hp > 0 && Monstre1.Hp > 0) {
 		fmt.Printf("Vous etes au tour %d\n", Tours)
+		goblinPatternActivated := GoblinPattern(Monstre1)
 
-		if P1.Initiative >= Monstre1.Initiative {
-			// Player's turn
-			charTurn(P1, Monstre1)
-		} else {
-			// Monster's turn
-			attaqueMonstre := Monstre1.Atk - P1.Defense
-			if attaqueMonstre < 0 {
-				attaqueMonstre = 0
-			}
-			P1.Hp -= attaqueMonstre
+		// Tour du Joueur 1
+		charTurn(P1, Monstre1)
+		attaqueJoueur := P1.Atk - Monstre1.Defense
+		if attaqueJoueur < 0 {
+			attaqueJoueur = 0
+		}
+		Monstre1.Hp -= attaqueJoueur
 
-			fmt.Printf("%s attaque %s et lui inflige %d points de dégâts.\n", Monstre1.Name, P1.Name, attaqueMonstre)
+		fmt.Printf("%s attaque %s et lui inflige %d points de dégâts.\n", P1.Name, Monstre1.Name, attaqueJoueur)
 
-			// Check if the Player is defeated
-			if P1.Hp <= 0 {
-				fmt.Printf("%s est vaincu!\n", P1.Name)
-				P1.Dead()
-				break
-			}
-
-			// Player's turn
-			charTurn(P1, Monstre1)
+		// Vérifier si le Monstre est toujours en vie
+		if Monstre1.Hp <= 0 {
+			fmt.Printf("%s est vaincu!\n", Monstre1.Name)
+			GagnerXp(P1, Monstre1)
+			break
 		}
 
+		// Tour du Monstre
+		attaqueJoueur2 := Monstre1.Atk - P1.Defense
+		if attaqueJoueur2 < 0 {
+			attaqueJoueur2 = 0
+		}
+		P1.Hp -= attaqueJoueur2
+
+		fmt.Printf("%s attaque %s et lui inflige %d points de dégâts.\n", Monstre1.Name, P1.Name, attaqueJoueur2)
 		Tours++
+		// Apply goblin's attack pattern if it was activated
+		if goblinPatternActivated {
+			// Reset goblin's attack back to its original value
+			Monstre1.Atk /= 2
+		}
 		fmt.Println("-----------------------------------------------------------------------------")
+
+		// Vérifier si le Joueur est toujours en vie
+		if P1.Hp <= 0 {
+			fmt.Printf("%s est vaincu!\n", P1.Name)
+			P1.Dead()
+			break
+		}
+	}
+	for (P1.Hp > 0 && Monstre1.Hp > 0 && Tours == 1 && P1.Initiative < Monstre1.Initiative) || (Tours > 1 && P1.Hp > 0 && Monstre1.Hp > 0) {
+		fmt.Printf("Vous etes au tour %d\n", Tours)
+		goblinPatternActivated := GoblinPattern(Monstre1)
+
+		// Tour du Monstre
+		attaqueJoueur2 := Monstre1.Atk - P1.Defense
+		if attaqueJoueur2 < 0 {
+			attaqueJoueur2 = 0
+		}
+		P1.Hp -= attaqueJoueur2
+
+		fmt.Printf("%s attaque %s et lui inflige %d points de dégâts.\n", Monstre1.Name, P1.Name, attaqueJoueur2)
+		Tours++
+		// Apply goblin's attack pattern if it was activated
+		if goblinPatternActivated {
+			// Reset goblin's attack back to its original value
+			Monstre1.Atk /= 2
+		}
+
+		// Vérifier si le Joueur est toujours en vie
+		if P1.Hp <= 0 {
+			fmt.Printf("%s est vaincu!\n", P1.Name)
+			P1.Dead()
+			break
+		} // Tour du Joueur 1
+		charTurn(P1, Monstre1)
+		attaqueJoueur := P1.Atk - Monstre1.Defense
+		if attaqueJoueur < 0 {
+			attaqueJoueur = 0
+		}
+		Monstre1.Hp -= attaqueJoueur
+
+		fmt.Printf("%s attaque %s et lui inflige %d points de dégâts.\n", P1.Name, Monstre1.Name, attaqueJoueur)
+
+		// Vérifier si le Monstre est toujours en vie
+		if Monstre1.Hp <= 0 {
+			fmt.Printf("%s est vaincu!\n", Monstre1.Name)
+			break
+		}
 	}
 }
 
@@ -83,9 +108,7 @@ func GetTours() int {
 
 func charTurn(P1 *Personnage, Monstre1 *Monstre) {
 	fmt.Printf("Vous avez %d Pv / %d Pv \n", P1.Hp, P1.HpMax)
-	fmt.Printf("Vous avez %d Pv / %d Pv \n", P1.Mana, P1.ManaMax)
 	fmt.Printf("Il reste %d Pv / %d Pv à l'ennemi \n", Monstre1.Hp, Monstre1.HpMax)
-	fmt.Printf("Il reste %d Pv / %d Pv à l'ennemi \n", Monstre1.Mana, Monstre1.ManaMax)
 
 	fmt.Println("C'est votre tour, que voulez vous faire ?")
 	fmt.Println("1 : Attaquer")
@@ -96,29 +119,11 @@ func charTurn(P1 *Personnage, Monstre1 *Monstre) {
 
 	switch choice {
 	case 1:
-		fmt.Println("Attaque basique")
-		attaqueJoueur := P1.Atk - Monstre1.Defense
-		if attaqueJoueur < 0 {
-			attaqueJoueur = 0
-		}
-		Monstre1.Hp -= attaqueJoueur
-		fmt.Printf("%s attaque %s et lui inflige %d points de dégâts.\n", P1.Name, Monstre1.Name, attaqueJoueur)
+		fmt.Println("Attaque basic")
 	case 2:
-		P1.FightInventory()
+		P1.FightInventory(Monstre1)
 	case 3:
-		fmt.Println("Sélectionnez un sort :")
-		for i, spell := range P1.Spells {
-			fmt.Printf("%d. %s (Type: %s, Dommages: %d, Coût en Mana: %d)\n", i+1, spell.Name, spell.Type, spell.Damage, spell.ManaCost)
-		}
-		var selectedSpellIndex int
-		fmt.Print("Entrez le numéro du sort que vous souhaitez lancer : ")
-		fmt.Scan(&selectedSpellIndex)
-		if selectedSpellIndex >= 1 && selectedSpellIndex <= len(P1.Spells) {
-			selectedSpell := P1.Spells[selectedSpellIndex-1]
-			P1.CastSpell(selectedSpell, Monstre1)
-		} else {
-			fmt.Println("Sélection de sort invalide.")
-		}
+
 	}
 }
 
