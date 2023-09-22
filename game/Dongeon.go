ead2603454edc5a3d9cf5de607132f0b9635d0bc
Alexis <alexis.clerc@ynov.com>
2023-09-22 15:56:34 +0200

    map

--- game/Dongeon.go
+++ game/Dongeon.go
@@ -31,11 +31,19 @@ func reverseDirection(direction string) string {
 }
 
 func Dongeon(P1 *Personnage) {
-	forest := NewRoom("Forêt", "Vous arrivé dans une forêt dense et mystérieuse", true)
-	entrance := NewRoom("Entrée", "Vous êtes à l'entrée du dongeon", false)
+	forest := NewRoom("Forêt", "Vous arrivez dans une forêt dense et mystérieuse", true)
+	entrance := NewRoom("Entrée", "Vous êtes à l'entrée du donjon", false)
 	hall := NewRoom("Hall", "Vous êtes dans un long hall", true)
 	treasureRoom := NewRoom("Salle au trésor", "Vous êtes dans une pièce remplie de trésors.", false)
-	directions := []string{"nord", "sud", "est", "ouest"}
+
+	// Définissez les sorties entre les salles
+	forest.AddExit("nord", entrance)
+	entrance.AddExit("sud", forest)
+	entrance.AddExit("est", hall)
+	hall.AddExit("ouest", entrance)
+	hall.AddExit("est", treasureRoom)
+	treasureRoom.AddExit("ouest", hall)
+
 	currentRoom := forest
 	monsterRooms := map[*Room]bool{
 		forest:       true,
@@ -43,49 +51,36 @@ func Dongeon(P1 *Personnage) {
 		hall:         true,
 		treasureRoom: false,
 	}
+
 	for {
 		fmt.Println(currentRoom.Description)
 		if monster, ok := monsterRooms[currentRoom]; ok && monster {
-			fmt.Println("Un monstre est apparu, préparez vous au combat !")
+			fmt.Println("Un monstre est apparu, préparez-vous au combat !")
 			Fight(P1, &Goblin)
 		}
 
 		if len(currentRoom.Exits) == 0 {
 			fmt.Println("Vous êtes bloqués, il n'y a aucune sortie de ce côté.")
-			continue
+			break
 		}
+
 		fmt.Print("Vous avez trouvé la sortie : ")
 		for direction := range currentRoom.Exits {
 			fmt.Printf("%s ", direction)
 		}
 		fmt.Println()
 
-		var chosenDirection int
-		fmt.Print("Choisissez une direction : 1.Nord, 2.Sud, 3.Est, 4.Ouest ")
-		_, err := fmt.Scanln(&chosenDirection)
+		var chosenDirection string
+		fmt.Print("Choisissez une direction : (nord, sud, est, ouest) ")
+		fmt.Scanln(&chosenDirection)
 
-		if err != nil {
-			fmt.Println("Invalid input. Please enter a number.")
-			continue
-		}
-
-		// Check if the chosen direction is valid
-		nextRoom, ok := currentRoom.Exits[directions[chosenDirection-1]]
+		// Vérifiez si la direction choisie est une sortie valide
+		nextRoom, ok := currentRoom.Exits[chosenDirection]
 		if !ok {
-			fmt.Println("Invalid direction. Try again.")
+			fmt.Println("Direction invalide. Veuillez réessayer.")
 			continue
 		}
 
-		// Move the player to the new room
-		currentRoom = nextRoom
-		if !ok {
-			fmt.Println("Invalid direction. Try again.")
-			continue
-		}
-		if nextRoom == nil {
-			fmt.Println("La sortie est bloquée. Choisissez une autre direction.")
-			continue
-		}
 		// Déplacez le joueur vers la nouvelle salle
 		currentRoom = nextRoom
 	}
