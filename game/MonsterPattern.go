49eebcc4c1be3beef0ada85de6a12f2d71c3993d
Nicolas <nicolas.moyon@ynov.com>
2023-09-29 10:04:13 +0200

    pull

--- game/MonsterPattern.go
+++ game/MonsterPattern.go
@@ -3,14 +3,10 @@ package game
 var GoblinAttackIncreased bool
 
 func MonsterPattern(Monstre1 *Monstre) {
-	// Check if the goblin's attack is currently increased
 	if GoblinAttackIncreased {
-		// Deactivate the increased attack
 		Goblin.Atk = Goblin.Atk / 2
 		GoblinAttackIncreased = false
 	}
-
-	// Reset goblin turn count every 3 turns
 	if Tours%3 == 0 {
 		Goblin.Atk = Goblin.Atk * 2
 		GoblinAttackIncreased = true
